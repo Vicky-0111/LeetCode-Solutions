56007ea82b847cbedcc7da8ab764f233ee315d08
Vicky-0111 <sanzujack7@gmail.com>
2026-06-06 17:56:44 +0530

    Added matrix diagonal sum

---
+++ Matrix/DiagonalSum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class DiagonalSum {
+    public static int printSum(int matrix[][]){
+
+        int sum = 0;
+        
+        for(int i = 0; i < matrix.length; i++)
+        {
+            //Primary Diagonal
+            sum += matrix[i][i];
+
+            //Secondary Diagonal
+            if(i != matrix.length-1-i)
+            {
+                sum += matrix[i][matrix.length-1-i];
+            }
+
+        }
+
+        return sum;
+    }
+    public static void main(String[] args){
+        
+        int matrix[][] = {{1,2,3,4},
+                         {5,6,7,8},
+                         {9,10,11,12},
+                         {13,14,15,16}};
+
+        System.out.print(printSum(matrix));
+    }
+}
